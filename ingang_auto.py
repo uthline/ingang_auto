25726b95ad68248aaa2e3c23f091c5cee6d84b3f
uthline <uthline@uthline.net>
2018-12-04 05:52:08 +0900

    add basic dialog
    load/show dialog.ui

--- ingang_auto.py
+++ ingang_auto.py
@@ -1,8 +1,22 @@
 import sys
 from PyQt5 import QtWidgets
+from PyQt5 import uic
+
+form_class = uic.loadUiType("dialog.ui")[0]
+
+class MyWindow(QtWidgets.QMainWindow, form_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+    def accept(self):
+        quit()
+
+    def reject(self):
+        pass
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    wind = QtWidgets.QMainWindow()
+    wind = MyWindow()
     wind.show()
     app.exec_()
