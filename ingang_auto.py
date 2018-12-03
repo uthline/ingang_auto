6b97676d74100bfe79372098aa80e1f50893f174
uthline <uthline@uthline.net>
2018-12-04 08:02:07 +0900

    add transparent window to setting region.

--- ingang_auto.py
+++ ingang_auto.py
@@ -1,22 +1,55 @@
 import sys
-from PyQt5 import QtWidgets
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 from PyQt5 import uic
+
+class WhiteWindow(QMainWindow):
+    def paintEvent(self, event=None):
+        painter = QPainter(self)
+
+        painter.setOpacity(0.7)
+        painter.setBrush(Qt.white)
+        painter.setPen(QPen(Qt.white))
+        painter.drawRect(self.rect())
 
 form_class = uic.loadUiType("dialog.ui")[0]
 
-class MyWindow(QtWidgets.QMainWindow, form_class):
+class MyWindow(QMainWindow, form_class):
+    sig_show_white = pyqtSignal()
+
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
+    def setting(self):
+        self.sig_show_white.emit()
+
     def accept(self):
         quit()
 
-    def reject(self):
-        pass
+class MyApplication(QApplication):
+    def __init__(self, argv):
+        super().__init__(argv)
+        self.wind = MyWindow()
+        self.wind.sig_show_white.connect(self.show_white_board)
+
+        # Create the main window
+        self.window = WhiteWindow()
+
+        self.window.setWindowFlags(Qt.FramelessWindowHint)
+        self.window.setAttribute(Qt.WA_NoSystemBackground, True)
+        self.window.setAttribute(Qt.WA_TranslucentBackground, True)
+
+    def start_application(self):
+        self.wind.show()
+
+    @pyqtSlot()
+    def show_white_board(self):
+        self.window.showFullScreen()
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    wind = MyWindow()
-    wind.show()
-    app.exec_()
+    app = MyApplication(sys.argv)
+    app.start_application()
+    sys.exit(app.exec_())
