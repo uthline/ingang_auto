2ab91990f2018dfcf7581f1e0f0d8024f1177975
uthline <uthline@uthline.net>
2018-12-04 08:59:32 +0900

    add mouse region setting function

--- ingang_auto.py
+++ ingang_auto.py
@@ -6,13 +6,47 @@ from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
 class WhiteWindow(QMainWindow):
+    sig_region_updated = pyqtSignal(QRect)
+
+    def __init__(self):
+        super().__init__()
+        self.pos_start = None
+        self.pos_end = None
+
     def paintEvent(self, event=None):
         painter = QPainter(self)
-
         painter.setOpacity(0.7)
         painter.setBrush(Qt.white)
         painter.setPen(QPen(Qt.white))
         painter.drawRect(self.rect())
+
+        if self.pos_start is not None:
+            rect = QRect(self.pos_start.x(), self.pos_start.y(),
+                         self.pos_end.x() - self.pos_start.x(),
+                         self.pos_end.y() - self.pos_start.y());
+            painter.setPen(QPen(Qt.red))
+            painter.drawRect(rect);
+
+    def mousePressEvent(self, QMouseEvent):
+        print(QMouseEvent.pos())
+        self.pos_end = self.pos_start = QMouseEvent.pos()
+        self.update()
+
+    def mouseReleaseEvent(self, QMouseEvent):
+        print(QMouseEvent.pos())
+        self.pos_end = QMouseEvent.pos()
+        self.close()
+
+    def mouseMoveEvent(self, QMouseEvent):
+        self.pos_end = QMouseEvent.pos()
+        if self.pos_start is not None:
+            self.update()
+
+    def closeEvent(self, QEvent):
+        rect = QRect(self.pos_start.x(), self.pos_start.y(),
+                     self.pos_end.x() - self.pos_start.x(),
+                     self.pos_end.y() - self.pos_start.y());
+        self.sig_region_updated.emit(rect)
 
 form_class = uic.loadUiType("dialog.ui")[0]
 
@@ -29,25 +63,33 @@ class MyWindow(QMainWindow, form_class):
     def accept(self):
         quit()
 
+    def update_region(self, rect):
+        self.pos_start_x.setText(str(rect.left()))
+        self.pos_start_y.setText(str(rect.top()))
+        self.pos_end_x.setText(str(rect.right()))
+        self.pos_end_y.setText(str(rect.bottom()))
+
 class MyApplication(QApplication):
     def __init__(self, argv):
         super().__init__(argv)
-        self.wind = MyWindow()
-        self.wind.sig_show_white.connect(self.show_white_board)
+        self.main_wind = MyWindow()
+        self.main_wind.sig_show_white.connect(self.show_white_board)
 
         # Create the main window
-        self.window = WhiteWindow()
+        self.white_wind = WhiteWindow()
 
-        self.window.setWindowFlags(Qt.FramelessWindowHint)
-        self.window.setAttribute(Qt.WA_NoSystemBackground, True)
-        self.window.setAttribute(Qt.WA_TranslucentBackground, True)
+        self.white_wind.setWindowFlags(Qt.FramelessWindowHint)
+        self.white_wind.setAttribute(Qt.WA_NoSystemBackground, True)
+        self.white_wind.setAttribute(Qt.WA_TranslucentBackground, True)
+
+        self.white_wind.sig_region_updated.connect(self.main_wind.update_region)
 
     def start_application(self):
-        self.wind.show()
+        self.main_wind.show()
 
     @pyqtSlot()
     def show_white_board(self):
-        self.window.showFullScreen()
+        self.white_wind.showFullScreen()
 
 if __name__ == "__main__":
     app = MyApplication(sys.argv)
